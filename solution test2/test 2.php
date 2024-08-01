801231ca21396b16bd9e3e8dae272aef14c22a1d
Tenzinom <987478@win.edu.au>
2024-08-01 16:29:42 +1000

    Add files via upload

---
+++ solution test2/test 2.php
@@ -0,0 +1,14 @@
+﻿
+
+<?php
+function findSingleNumber($arr) {
+$numCounts = array();
+// Count the occurrences of each number
+foreach ($arr as $num) {
+if (isset($numCounts[$num])) {
+$numCounts[$num]++;
+} else {
+$numCounts[$num] = 1;
+}
+}
+// Find the number that occurs only once
